41fbf40f4c81d8efd2b0de218e466f2ed630cd82
Sebastian Lopez <seb.lopez331@gmail.com>
2025-04-21 01:04:24 -0400

    feat: finalized auth

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -235,11 +235,13 @@ exports.login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production' ? true : false,
       sameSite: process.env.NODE_ENV === 'production' ? 'Strict' : 'Lax',
+      maxAge: 15 * 60 * 1000, // 15 minutes
     })
     .cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production' ? true : false,
       sameSite: process.env.NODE_ENV === 'production' ? 'Strict' : 'Lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     .json({ message: 'Login successful' });
 };
